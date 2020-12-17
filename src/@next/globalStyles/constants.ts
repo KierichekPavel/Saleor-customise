580a7f38cbb01991fbdc9fd3333e902456f7cf86
Paul Kirichek <53315498+KierichekPavel@users.noreply.github.com>
2020-12-17 21:52:02 +0200

    Adding new stiles

---
+++ src/@next/globalStyles/constants.ts
@@ -0,0 +1,95 @@
+// colors
+export const autofillColor = "rgb(250, 255, 189)";
+export const autofillColorSelected = "rgb(232, 240, 254)";
+export const baseFontColor = "#303030";
+export const baseFontColorSemiTransparent = "rgba(50,50,50,0.6)";
+export const baseFontColorTransparent = "rgba(50,50,50,0.1)";
+export const black = "#000";
+export const red = "#EF4A5A";
+export const redDark = "#BE1E2D";
+export const redLight = "#FF9EA7";
+export const blueOverlay = `rgba(33,18,94,0.1)`;
+export const blueOverlayDark = `rgba(33,18,94,0.2)`;
+export const gray = "#B1AEAD";
+export const grayMedium = "#c4c4c4";
+export const grayDark = "#7d7d7d";
+export const grayLight = "#F7F7F7";
+export const greensucces = "#3ed256";
+export const overlayColor = "rgba(199, 207, 207, 0.8)";
+export const rose = "#c22d74";
+export const green = "#87B64F";
+export const greenDark = "#52A25E";
+export const greenLight = "rgba(139, 202, 132, 0.25)";
+export const greenTransparent = "rgba(139, 202, 132, 0.1)";
+export const white = "#fff";
+export const tabelGray = "#eaeaea";
+export const darkGreen = "#1E4B25";
+export const secondaryGrey = "#EBF1F6";
+export const secondaryGreyDark = "#9D9FB1";
+export const secondaryBlue = "#036DFF";
+
+// theme colors
+export const theme = {
+  activeMenuOption: darkGreen,
+  autofill: autofillColor,
+  autofillSelected: autofillColorSelected,
+  bannerBackground: secondaryGrey,
+  bannerEdge: secondaryGreyDark,
+  bannerLink: secondaryBlue,
+  baseFont: baseFontColor,
+  baseFontColorSemiTransparent,
+  baseFontColorTransparent,
+  dark: black,
+  disabled: grayLight,
+  divider: grayLight,
+  dividerDark: grayMedium,
+  error: rose,
+  hoverLightBackground: greenLight,
+  light: white,
+  lightFont: gray,
+  listAttributeName: baseFontColorSemiTransparent,
+  listBullet: darkGreen,
+  overlay: overlayColor,
+  primary: green,
+  primaryDark: greenDark,
+  primaryLight: greenLight,
+  primaryTransparent: greenTransparent,
+  secondary: red,
+  secondaryDark: redDark,
+  secondaryLight: redLight,
+  secondaryOverlay: blueOverlay,
+  secondaryOverlayDark: blueOverlayDark,
+  success: greensucces,
+  tabTitle: darkGreen,
+  tableDivider: tabelGray,
+  tabsBorder: baseFontColorTransparent,
+  thumbnailBorder: darkGreen,
+  white,
+};
+
+// typography
+export const baseFontFamily = "'Inter', sans-serif";
+export const baseFontSize = "1rem"; // 16px
+export const baseLineHeight = "1.25rem"; // 20px
+export const boldFontWeight = 600;
+export const extraBoldFontWeight = 800;
+export const h1FontSize = "4rem"; // 64px
+export const h2FontSize = "3rem"; // 48px
+export const h1LineHeight = 1;
+export const h3FontSize = "1.5rem"; // 24px
+export const h4FontSize = "1.125rem"; // 18px
+export const labelFontSize = "0.75rem"; // 12px
+export const smallFontSize = "0.875rem"; // 14px
+export const ultraBigFont = "6rem"; // 96px
+
+// spacing
+export const spacer = 1; // rem
+export const fieldSpacer = "1.875rem";
+
+// breakpoints
+export const xxxLargeScreen = 1920;
+export const xxLargeScreen = 1600;
+export const xLargeScreen = 1280;
+export const largeScreen = 992;
+export const mediumScreen = 720;
+export const smallScreen = 540;
